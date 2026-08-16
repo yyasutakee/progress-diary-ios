acdd61d9e23da5833a87110c0aa5220ebcfad044
Ystk [GitHub] <youhey.sama@gmail.com>
2026-08-17 02:48:40 +0900

    Initial commit

---
+++ iOS-Template/iOS-Template/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iOS-Template
+//
+//  Created by Yuhei Yasutake on 2026/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ iOS-Template/iOS-Template/iOS_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TemplateApp.swift
+//  iOS-Template
+//
+//  Created by Yuhei Yasutake on 2026/08/17.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
