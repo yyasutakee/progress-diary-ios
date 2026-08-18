d90902e45cf29c6d4e8a545851bcc872f5d5c039
Yuhei Yasutake <yyasutakee@gmail.com>
2026-08-19 00:49:32 +0900

    DiaryFeatureをiOS 26対応に更新
    Liquid GlassのglassEffect APIを利用できるようパッケージの最低対応OSをiOS 26へ変更する。

--- iOS-Template/Packages/DiaryFeature/Package.swift
+++ iOS-Template/Packages/DiaryFeature/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "DiaryFeature",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v26)],
     products: [
         .library(name: "DiaryFeature", targets: ["DiaryFeature"])
     ],
